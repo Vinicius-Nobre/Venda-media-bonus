dfda559e77d564db0507b51c2a9a1c488c92ac60
Ti <Ti@DESKTOP-7K4A3P9>
2022-04-14 10:43:55 -0300

    Adicionar arquivos de projeto.

---
+++ Venda-media-bonus/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Teste_media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nome;
+            int vendaum;
+            int vendadois;
+            int vendatres;
+            int vendaquatro;
+            int media;
+
+
+            Console.WriteLine("Digite o nome do vendedor");
+            nome = (Console.ReadLine());
+
+            Console.WriteLine("Digite a primeira venda");
+            vendaum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda venda");
+            vendadois = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a terceira venda");
+            vendatres = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite a quarta venda");
+            vendaquatro = Convert.ToInt32(Console.ReadLine());
+
+
+            media = ((vendaum + vendadois + vendatres + vendaquatro) / 4);
+            Console.WriteLine("Sua media de venda é" + media);
+
+            if (media >= 7)
+            {
+                Console.WriteLine(nome + "Ganhou o bônus");
+
+            }
+
+            else
+            {
+                Console.WriteLine(nome + "não ganhou bônus");
+            }
+
+
+
+        }
+    }
+}
